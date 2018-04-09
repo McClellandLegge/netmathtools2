5f1718bf897790efe0ddfc7c7d691ea619c9dc39
McClelland Legge <McClellandLegge@users.noreply.github.com>
2018-04-09 14:58:04 -0500

    adding workflow with activity

---
+++ R/mentor-activity.R
@@ -0,0 +1,122 @@
+#' Title
+#'
+#' @param handle
+#' @param students
+#'
+#' @return
+#' @export
+#' @imports purrr
+#'
+getMentorActivity <- function(handle, students) {
+
+  tickets <- list()
+
+  for (student_id in students[['student_netid']]) {
+    page <- 1L
+    tickets[[paste0(student_id)]] <- list()
+    while (TRUE) {
+
+      # get all the conversation tickets on the first page
+      conversations <- netmathtools2::getTicketList(handle, student_id, page = page)
+
+      # if there are no conversations on this page then we've reached the end
+      # and there are no tickets from the student
+      if (conversations$count == 0L) {
+        break
+      }
+
+      # grab the conversation ids
+      convo_ids <- sapply(conversations[["results"]], `[[`, "id", USE.NAMES = FALSE)
+
+      # request the tickets, which include all of the messages
+      convo_msgs <- lapply(convo_ids, netmathtools2::getTicketMessages, handle = handle)
+
+      # combine the messages, we only care about the date, not convo ownership
+      all_msg <- unlist(lapply(convo_msgs, `[[`, "results"), recursive = FALSE)
+
+      # filter the inbound messages, we want when a student has last emailed
+      outbound_msg <- Filter(function(msg) msg$is_outgoing == 1, all_msg)
+
+      dates <- map(outbound_msg, 'created') %>%
+        map(anytime::anytime) %>%
+        do.call(c, .)
+
+      tickets[[paste0(student_id)]] <- c(dates, tickets[[paste0(student_id)]])
+
+      page <- page + 1L
+    }
+  }
+
+  return(tickets)
+
+}
+
+
+
+#' Get the details about a Mathable assignment
+#'
+#' @param netid
+#' @param course_id
+#'
+#' @return
+#' @export
+getAsssignmentDetail <- function(netid, course_id) {
+
+  # strip out the deployed course ID and student netID
+  info     <- strsplit(course_id, "_(?=[^_]+$)", perl = TRUE)
+  deployed <- info[[1]][[1]] %>% gsub("studentcourserecords", "deployedcourses", .)
+  s_netid  <- info[[1]][[2]]
+
+  # handle for this student's processing
+  h <- composeMathableHandle(netid)
+
+  res <- netmathtools2::getRequest(
+    handle   = h
+    , where    = "mathable"
+    , route    = "GetGradebook"
+    , courseId = paste0('"', deployed, '"')
+  )
+
+  nbs <- res$d$fields %>%
+    grep("GiveItaTry|Literacy", ., value = TRUE)
+
+  graded_assignments <- map(nbs, getMathableNotebook, handle = h, student_netid = s_netid) %>%
+    discard(is.null) %>%
+    map(function(x) {
+      attempt  <- x[['Attempt']]
+      graded   <- x[['DateGraded']]
+      submit   <- x[['SubmitDate']]
+      unit     <- x[['UnitNumber']]
+      tryit    <- x[['NotebookTitle']]
+
+      if (is.null(attempt) || is.null(graded)) {
+        return(NULL)
+      }
+
+      data.table(
+        attempt  = attempt
+        , graded   = graded
+        , submit   = submit
+        , unit     = unit
+        , tryit    = tryit
+      )
+    }) %>%
+    discard(is.null) %>%
+    rbindlist()
+
+  if (nrow(graded_assignments) > 0) {
+    graded_assignments[, `:=`(
+      graded  = as.POSIXct(graded, tz = "UTC", format = "%Y-%m-%dT%H:%M:%OS")
+      , submit  = as.POSIXct(submit, tz = "UTC", format = "%Y-%m-%dT%H:%M:%OS")
+      , student = s_netid
+    )]
+
+    return(graded_assignments)
+  } else {
+    return(NULL)
+  }
+}
+
+
+
+
---
+++ example-workflow.R
@@ -0,0 +1,101 @@
+library("netmathtools2")
+library("data.table")
+library("plotly")
+
+netid <- "mkemp6"
+
+tries <- 0L
+students <- NULL
+while (is.null(students)) {
+
+  h <- composeNexusHandle(netid)
+
+  # students <- getStudents(h, netid)
+  students <- tryCatch(getStudents(h, netid), error = function(e) NULL)
+
+  tries <- tries + 1
+  cat(paste("\rTried", tries, "times"))
+}
+
+tries <- 0L
+student_prog <- NULL
+while (is.null(student_prog)) {
+
+  h <- composeNexusHandle(netid)
+
+  # student_prog <- getStudentsProgress(h, students)
+  student_prog <- tryCatch(getStudentsProgress(h, students), error = function(e) NULL)
+
+  tries <- tries + 1
+  cat(paste("\rTried", tries, "times"))
+}
+
+customized_student_prog <- student_prog[, .(
+  full
+  , end_days
+  , orientation_date
+  , timeline
+  , has_proctor
+  , days_last_mentor_email
+  , days_last_student_email
+  , days_behind
+  , tryits_behind
+  , lessons_behind
+  , current_pace_interp
+  , needed_pace_interp
+  , completed_assignments
+  , exams
+  , end_date
+)]
+
+
+chartProgress(customized_student_prog)
+
+
+# Mentor Activity ---------------------------------------------------------
+
+
+
+emails     <- getMentorActivity(h, students)
+all_emails <- unlist(emails, use.names = FALSE)
+all_dates  <- data.table(date = anytime::anydate(all_emails))
+date_sum   <- all_dates[, .(emails = .N), by = date][order(date)]
+
+
+
+# Course ------------------------------------------------------------------
+
+
+system.time({
+  all_graded_assignments <- lapply(students$mathable_course_id, getAsssignmentDetail, netid = netid)
+})
+
+aga_dt <- rbindlist(all_graded_assignments)
+aga_dt[, `:=`(date = anytime::anydate(graded))]
+
+aga_sum <- aga_dt[, .(mathable = .N), by = date]
+
+plot_dat <- merge(date_sum, aga_sum, by = "date", all = TRUE)
+
+plot_dat[is.na(plot_dat)] <- 0
+
+plot_dat[, `:=`(total = 15 * emails + 7 * mathable)]
+
+m_plot_dat <- melt(plot_dat, id.vars = c("date", "total"), variable.factor = TRUE)
+
+bw_ending <- as.Date("2018-04-07", format = "%Y-%m-%d")
+bw_period <- seq(bw_ending - 13, bw_ending, by = "day")
+
+bw_plot <- m_plot_dat[date %in% bw_period]
+bw_totals <- unique(bw_plot[, .(date, total)])
+ggplot(bw_plot, aes(x = date, y = value, fill = variable)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_x_date(date_breaks = "day", date_labels = "%a, %b %d") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
+  geom_text(data = bw_totals, aes(x = date, y = 0, label = total), inherit.aes = FALSE)
+
+sum(bw_totals$total) / 60
+
+
+
+ggplotly(p)
